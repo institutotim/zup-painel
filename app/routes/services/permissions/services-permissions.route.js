db7b95f0253041b91a4806705b95c6af08c3f03e
lafabbrica <antonio.augusto.castanheira@gmail.com>
2017-03-15 11:47:31 -0300

    Cascavel theme

--- app/routes/services/permissions/services-permissions.route.js
+++ app/routes/services/permissions/services-permissions.route.js
@@ -8,15 +8,19 @@ angular
 
   .config(['$stateProvider', function ($stateProvider) {
 
-    $stateProvider
-
-      .state('services.show.permissions', {
-        url: '/permissions',
-        resolve: {
-          'Authorize': ['Authorization', 'User', function (Authorization, User) {
-            return Authorization.authorize(['manage_services'], null, true);
-          }]
-        },
+    $stateProvider.state('services.show.permissions', {
+      url: '/permissions',
+      resolve: {
+        'Authorize': [
+          'Authorization', '$stateParams', 'User',
+          function (Authorization, $stateParams) {
+            return Authorization.authorize(
+              ['groups_full_access', 'group_edit'],
+              $stateParams.id, true
+            );
+          }
+        ]
+      },
         views: {
           '@services': {
             templateUrl: 'routes/groups/edit/groups-edit.template.html',
@@ -24,11 +28,14 @@ angular
             controllerAs: 'ctrl',
             resolve: {
               'permissionsResponse': ['FullResponseRestangular', '$stateParams', function (FullResponseRestangular, $stateParams) {
-                return FullResponseRestangular.all('permissions').one('services', $stateParams.id).customGET();
+              return FullResponseRestangular.one('groups', $stateParams.id).all('permissions').customGET();
               }],
               'objectsResponse': ['FullResponseRestangular', function (FullResponseRestangular) {
                 return FullResponseRestangular.one('utils').all('available_objects').customGET();
-              }]
+              }],
+              'groupResponse': ['Restangular', '$stateParams', function(Restangular, $stateParams) {
+              return Restangular.one('groups', $stateParams.id).get();
+            }]
             }
           }
         }
